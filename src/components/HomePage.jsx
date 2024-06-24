0dc8a9ba9ca7a9e5e511c016ccac757d92ca1e9c
SaraKulsum <sarakhanx63@gmail.com>
2024-06-25 03:09:01 +0530

    fixed 3rd project image and portfolio title

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import Layout from './Layout'
 import HeroSection from './HeroSection'
 import project1 from "./project_Images/hamd.png";
 import project2 from "./project_Images/weather-app.png";
-
+import project3 from "./project_Images/Netflix.png"
 
 import ProjectDisplayFrame from './ProjectDisplayFrame'
 import LocomotiveScroll from 'locomotive-scroll';
@@ -21,7 +21,7 @@ const HomePage = () => {
           <HeroSection />
           <ProjectDisplayFrame heading = {"HAMD"} subLine = {"E-commerce web app"} img = {project1} link = {"https://hamd-ecom.web.app"} cls = {"project_hamd"}/>
           <ProjectDisplayFrame heading = {"Weather-App"} subLine = {"Provides Current Weather, 3-Hourly Forecast, Daily Forecast..."} img = {project2} link = {"https://weather-app-7860.web.app"}  cls = {"project_weatherApp"}/>
-          <ProjectDisplayFrame heading = {"Netflix-clone"} subLine = {"Provides Current Weather, 3-Hourly Forecast, Daily Forecast..."} img = {project2} link = {"https://hamd-ecom.web.app"}  cls = {"project_Netflix"}/>
+          <ProjectDisplayFrame heading = {"Netflix-clone"} subLine = {"Provides Current Weather, 3-Hourly Forecast, Daily Forecast..."} img = {project3} link = {"https://hamd-ecom.web.app"}  cls = {"project_Netflix"}/>
 
 
         </div>
